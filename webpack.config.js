bf2c72988799233a0e2f63bedde15fac8d3a55d7
Antonios Karagiannis <antokarag@gmail.com>
2017-10-23 00:17:47 +0000

    add webpack config, webpack plugins and src dir with files

---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+const path = require('path'),
+    HtmlWebpackPlugin = require('html-webpack-plugin'),
+    CleanWebpackPlugin = require('clean-webpack-plugin'),
+    FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new FaviconsWebpackPlugin({
+            logo: './src/static/logo.png',
+            prefix: 'icons/[hash]/',
+            emitStats: false,
+            statsFilename: 'icons/stats-[hash].json',
+            persistentCache: true,
+            inject: true,
+            background: '#fff',
+            // @see https://github.com/haydenbleasel/favicons#usage
+            title: 'Antonios Karagiannis',
+            // @see https://github.com/haydenbleasel/favicons#usage
+            icons: {
+                android: true,
+                appleIcon: true,
+                appleStartup: true,
+                coast: false,
+                favicons: true,
+                firefox: true,
+                opengraph: true,
+                twitter: true,
+                yandex: false,
+                windows: false
+            }
+        }),
+        new HtmlWebpackPlugin({
+            title: 'Antonios Karagiannis',
+            minify: {
+                minifyCSS: true,
+                minifyJS: true,
+                minifyURLs: true,
+                removeComments: true,
+                sortClassName: true,
+                useShortDoctype: true
+            }
+        })
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [{
+                test: /\.css$/,
+                use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader' }
+                ]
+            },
+            // {
+            //     test: /\.html$/,
+            //     use: ['file-loader?name=[path][name].[ext]!extract-loader!html-loader']
+            // },
+            // {
+            //     test: /\.(html)$/,
+            //     use: {
+            //         loader: 'html-loader',
+            //         options: {
+            //             attrs: [':data-src'],
+            //             minimize: true
+            //         }
+            //     }
+            // },
+            // {
+            //     test: /\.(png|jpg|gif)$/,
+            //     use: [{
+            //         loader: 'file-loader',
+            //         options: {}
+            //     }]
+            // }
+        ]
+    }
+};
