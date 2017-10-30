05ba83d4235586501d32cb4c766b2aadd9259d97
Antonios Karagiannis <antokarag@gmail.com>
2017-10-29 20:42:34 -0400

    add UglifyJSPlugin and use it in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,103 +3,110 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const HtmlWebpackTemplatePlugin = require('html-webpack-template');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
-module.exports = (env => ({
-  entry: './src/index.jsx',
-  devtool: ((env && env.production) ? false : 'source-map'),
-  devServer: {
-    // @see https://webpack.js.org/configuration/dev-server
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 9000,
-    https: false,
-  },
-  plugins: [
-    new CleanWebpackPlugin(['dist'], { exclude: ['icons'] }),
-    new FaviconsWebpackPlugin({
-      logo: './src/static/logo.png',
-      prefix: 'icons/[hash]/',
-      emitStats: false,
-      statsFilename: 'icons/stats-[hash].json',
-      persistentCache: true,
-      inject: true,
-      background: '#fff',
-      title: 'Antonios Karagiannis',
-      // @see https://github.com/haydenbleasel/favicons#usage
-      icons: {
-        android: true,
-        appleIcon: true,
-        appleStartup: true,
-        coast: false,
-        favicons: true,
-        firefox: true,
-        opengraph: true,
-        twitter: true,
-        yandex: false,
-        windows: false,
-      },
-    }),
-    new HtmlWebpackPlugin({
-      title: 'Antonios Karagiannis',
-      minify: {
-        minifyCSS: true,
-        minifyJS: true,
-        minifyURLs: true,
-        removeComments: true,
-        sortClassName: true,
-        useShortDoctype: true,
-        collapseWhitespace: true,
-      },
-      inject: false,
-      template: HtmlWebpackTemplatePlugin,
-      mobile: true,
-      lang: 'en-US',
-      appMountId: 'root',
-    }),
-  ],
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [{
-      test: /\.css$/,
-      use: [
-        { loader: 'style-loader' },
-        { loader: 'css-loader' },
-      ],
+module.exports = (env) => {
+  const isProdEnv = (env && env.production);
+  return {
+    entry: './src/index.jsx',
+    devtool: (isProdEnv ? false : 'source-map'),
+    devServer: {
+      // @see https://webpack.js.org/configuration/dev-server
+      contentBase: path.join(__dirname, 'dist'),
+      compress: true,
+      port: 9000,
+      https: false,
     },
-    {
-      test: /\.jsx?$/,
-      exclude: /(node_modules|bower_components)/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['es2015', 'react'],
+    plugins: [
+      new CleanWebpackPlugin(['dist'], { exclude: ['icons'] }),
+      new FaviconsWebpackPlugin({
+        logo: './src/static/logo.png',
+        prefix: 'icons/[hash]/',
+        emitStats: false,
+        statsFilename: 'icons/stats-[hash].json',
+        persistentCache: true,
+        inject: true,
+        background: '#fff',
+        title: 'Antonios Karagiannis',
+        // @see https://github.com/haydenbleasel/favicons#usage
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          opengraph: true,
+          twitter: true,
+          yandex: false,
+          windows: false,
+        },
+      }),
+      new HtmlWebpackPlugin({
+        title: 'Antonios Karagiannis',
+        minify: {
+          minifyCSS: true,
+          minifyJS: true,
+          minifyURLs: true,
+          removeComments: true,
+          sortClassName: true,
+          useShortDoctype: true,
+          collapseWhitespace: true,
+        },
+        inject: false,
+        template: HtmlWebpackTemplatePlugin,
+        mobile: true,
+        lang: 'en-US',
+        appMountId: 'root',
+      }),
+      new UglifyJSPlugin({
+        sourceMap: !isProdEnv,
+      }),
+    ],
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+      rules: [{
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+        ],
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015', 'react'],
+          },
         },
       },
+        // {
+        //     test: /\.html$/,
+        //     use: ['file-loader?name=[path][name].[ext]!extract-loader!html-loader']
+        // },
+        // {
+        //     test: /\.(html)$/,
+        //     use: {
+        //         loader: 'html-loader',
+        //         options: {
+        //             attrs: [':data-src'],
+        //             minimize: true
+        //         }
+        //     }
+        // },
+        // {
+        //     test: /\.(png|jpg|gif)$/,
+        //     use: [{
+        //         loader: 'file-loader',
+        //         options: {}
+        //     }]
+        // }
+      ],
     },
-      // {
-      //     test: /\.html$/,
-      //     use: ['file-loader?name=[path][name].[ext]!extract-loader!html-loader']
-      // },
-      // {
-      //     test: /\.(html)$/,
-      //     use: {
-      //         loader: 'html-loader',
-      //         options: {
-      //             attrs: [':data-src'],
-      //             minimize: true
-      //         }
-      //     }
-      // },
-      // {
-      //     test: /\.(png|jpg|gif)$/,
-      //     use: [{
-      //         loader: 'file-loader',
-      //         options: {}
-      //     }]
-      // }
-    ],
-  },
-}));
+  };
+};
