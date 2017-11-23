268f9f8fa2e5bb31ae4af25410eedc567c1fd89d
Antonios Karagiannis <antokarag@gmail.com>
2017-11-22 21:42:50 -0500

    feat(index.jsx): add MyGrid functional component and use it

--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
 
-// class myApp extends React.Component {
-
-// }
+function MyGrid() {
+  return (
+    <div className="ColumnSection__grid u-clearfix">
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>4</div>
+      <div>5</div>
+      <div>6</div>
+    </div>
+  );
+}
 
 ReactDOM.render(
-  <div><h1>Hello, world!</h1><a href="/home">home</a></div>,
+  <div>
+    <div className="u-clearfix">
+      <h1>Hello, world!</h1><a href="/home">home</a>
+    </div>
+    <MyGrid />
+  </div>,
   document.getElementById('root'),
 );
 
