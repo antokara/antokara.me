2f35ea0e6b4db8b3f6ae28dd90d1bd1c065edbf9
Antonios Karagiannis <antokarag@gmail.com>
2017-11-26 12:54:12 -0500

    feat(components/layout/header.jsx): add a Header component for the layout

---
+++ src/components/layout/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => (
+  <header className="layout ColumnSection__grid u-clearfix">
+    <div>
+      <div className="name"> name </div>
+    </div>
+  </header>
+);
+
+export default Header;
