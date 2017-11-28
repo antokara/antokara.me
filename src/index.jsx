33e01e383187a84d3498d98ad7c8cf978b863b82
Antonios Karagiannis <antokarag@gmail.com>
2017-11-27 22:30:24 -0500

    feat(index.jsx): add basic contentful support for use by header

--- src/index.jsx
+++ src/index.jsx
@@ -3,10 +3,16 @@ import {
   Route,
   Link,
 } from 'react-router-dom';
+import { createClient } from 'contentful';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/layout/header';
 import './css/index.css';
+
+const client = createClient({
+  space: 'lihy1bja3cqb',
+  accessToken: 'c144c3fa68c18c2a2d43f6796bc092fc6dbafa97be71d39cd2b0c244c8b5a32a',
+});
 
 const Home = () => (
   <div>
@@ -23,7 +29,7 @@ const About = () => (
 ReactDOM.render(
   <Router>
     <div>
-      <Header />
+      <Header client={client} />
       <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
