ba70d13885113eb5ce2db5f57689fda7afe2da5d
Antonios Karagiannis <antokarag@gmail.com>
2017-11-27 22:29:47 -0500

    feat(components/layout/header.jsx): add contentful support to it

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { ClientAPI } from 'contentful';
 
-const Header = () => (
-  <header className="layout ColumnSection__grid u-clearfix">
-    <div>
-      <div className="name"> name </div>
-    </div>
-  </header>
-);
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.client = props.client;
+    this.state = { name: null };
+    this.client.getEntry('6twmIWqENi6muimaEsEESu')
+      .then((entry) => {
+        this.setState({ name: entry.fields.first });
+      })
+      .catch((error) => {
+        // @todo use package for error reporting or push state up to app
+        // eslint-disable-next-line no-console
+        console.error(error);
+      });
+  }
+
+  render() {
+    return (
+      <header className="layout row">
+        <div>
+          <div className="name"> {this.state.name} </div>
+        </div>
+      </header>
+    );
+  }
+}
+
+Header.propTypes = {
+  client: PropTypes.objectOf(ClientAPI).isRequired,
+};
 
 export default Header;
