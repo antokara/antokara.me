25fbb8514e94638cebe1c98fe36f9bbd3e769c59
Antonios Karagiannis <antokarag@gmail.com>
2017-12-07 22:50:18 -0500

    feat(config/app): add app config files

---
+++ config/app/production.js
@@ -0,0 +1,8 @@
+const config = {
+  contentful: {
+    space: 'lihy1bja3cqb-production',
+    accessToken: 'c144c3fa68c18c2a2d43f6796bc092fc6dbafa97be71d39cd2b0c244c8b5a32a',
+  },
+};
+
+export default config;
