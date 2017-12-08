5c7bdb20632c9e4c5d956cbd63cbd4e64b2ba0bb
Antonios Karagiannis <antokarag@gmail.com>
2017-12-07 21:36:27 -0500

    feat(config/jest): add Jest configuration

---
+++ config/jest.js
@@ -0,0 +1,8 @@
+// @see https://facebook.github.io/jest/docs/en/getting-started.html
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+// @see https://github.com/facebookincubator/create-react-app/issues/3199
+// warning remains but might help in future upgrade
+import 'raf/polyfill';
+
+Enzyme.configure({ adapter: new Adapter() });
