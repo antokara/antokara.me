387a422197ad3bff0ab99cec39bfb74e4d264bb3
Antonios Karagiannis <antokarag@gmail.com>
2017-12-07 21:15:41 -0500

    feat(components/App): add App component

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Routes from './Routes';
+import app from '../css/app.pcss';
+
+const App = () => (
+  <div className={app.app}>
+    <h1>App</h1>
+    <hr />
+    <Link to="/">Home</Link>
+    <Link to="/about">About</Link>
+    <hr />
+    <Routes />
+  </div>
+);
+
+export default App;
