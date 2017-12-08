a9ca78be50a62d86668bfb9273d8e366cd92c524
Antonios Karagiannis <antokarag@gmail.com>
2017-12-07 21:20:39 -0500

    feat(index.jsx): change to use redux store

--- src/index.jsx
+++ src/index.jsx
@@ -1,45 +1,17 @@
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-} from 'react-router-dom';
-import { createClient } from 'contentful';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Header from './components/layout/header';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux';
+import App from './components/App';
+import store from './helpers/store';
+import history from './helpers/browserHistory';
 import './css/index.pcss';
 
-// @todo move that into app component or something
-const client = createClient({
-  space: 'lihy1bja3cqb',
-  accessToken: 'c144c3fa68c18c2a2d43f6796bc092fc6dbafa97be71d39cd2b0c244c8b5a32a',
-});
-
-const Home = () => (
-  <div>
-    <h2>Home</h2>
-  </div>
-);
-
-const About = () => (
-  <div>
-    <h2>About</h2>
-  </div>
-);
-
-ReactDOM.render(
-  <Router>
-    <div>
-      <Header client={client} />
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-      </ul>
-      <hr />
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-    </div>
-  </Router>,
+render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>,
   document.getElementById('root'),
 );
-
