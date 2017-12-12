3181b39fb0ec6f7cf2052893924ddbc4cc16ac90
Antonios Karagiannis <antokarag@gmail.com>
2017-12-11 22:37:38 -0500

    perf(components/Home): remove appConfig

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import appConfig from 'appConfig';
 import style from '../css/home.pcss';
 
 const Home = () => (
   <div className={style.home}>
-    Home, contentful space from config: {appConfig.contentful.space}
+    Home
   </div>
 );
 
