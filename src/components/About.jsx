64df80fd51b1f6baaae80e72d3b000f7f51d4dc5
Antonios Karagiannis <antokarag@gmail.com>
2017-12-11 22:34:52 -0500

    refactor(components/About): replace appConfig with process.env

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import appConfig from 'appConfig';
 import style from '../css/about.pcss';
 
 const About = () => (
   <div className={style.about}>
-    <div>{appConfig.contentful.space}</div>
+    <div data-id="space">{process.env.CONTENTFUL_SPACE}</div>
     <div data-id="inner">
       About
     </div>
