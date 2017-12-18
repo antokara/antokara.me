3c95c60a58d45421d5d9fa49957587aa2058a749
Antonios Karagiannis <antokarag@gmail.com>
2017-12-17 21:31:36 -0500

    feat(constants/routes): add routes constant object

---
+++ src/constants/routes.js
@@ -0,0 +1,12 @@
+/**
+ * our Routes defined as "key":"path"
+ * few reasons why this constant object exists are
+ *  - easier URL/path redirection in the future if needed
+ *  - ability to map content such as "menu title" dynamically using the "key"
+ */
+const routes = {
+  home: '/',
+  about: '/about',
+};
+
+export default routes;
