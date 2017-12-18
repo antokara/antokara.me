4c6acebf9382222c4272c4dfab884050368f85e2
Antonios Karagiannis <antokarag@gmail.com>
2017-12-17 21:32:29 -0500

    refactor(components/Routes.jsx): refactor to use routes constant object for paths

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { Route } from 'react-router';
 import Home from 'Components/Home.jsx';
 import About from 'Components/About.jsx';
+import routes from 'Constants/routes';
 
 const Routes = () => (
   <div>
-    <Route exact path="/" component={Home} />
-    <Route path="/about" component={About} />
+    <Route exact path={routes.home} component={Home} />
+    <Route path={routes.about} component={About} />
   </div>
 );
 
