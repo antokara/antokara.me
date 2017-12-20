0540c9fd5faa76e5f18b87731d0aea06dd04d717
Antonios Karagiannis <antokarag@gmail.com>
2017-12-19 22:53:55 -0500

    feat(index.jsx): import global index style and refactor App.jsx import path

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
-import App from 'Components/App.jsx';
+import App from 'Containers/App.jsx';
 import store from 'Helpers/store';
 import history from 'Helpers/browserHistory';
+import './index.pcss';
 
 render(
   <Provider store={store}>
