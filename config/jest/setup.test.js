c6c5f255925ae9a571c3c6d9b9e33befcec64233
Antonios Karagiannis <antokarag@gmail.com>
2017-12-19 21:37:56 -0500

    test(config/jest/setup.test.js): add promise/fetch polyfill in Jest setup and enable fetch mock

--- config/jest/setup.test.js
+++ config/jest/setup.test.js
@@ -1,10 +1,10 @@
 // @see https://facebook.github.io/jest/docs/en/getting-started.html
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-// @see https://github.com/facebookincubator/create-react-app/issues/3199
-// warning remains but might help in future upgrade
-import 'raf/polyfill';
 
 Enzyme.configure({ adapter: new Adapter() });
 
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
+global.fetch = require('jest-fetch-mock');
 require('dotenv').config();
