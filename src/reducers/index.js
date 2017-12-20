a45cb1fd0ec60ca266a65c780efcd03def2eadc5
Antonios Karagiannis <antokarag@gmail.com>
2017-12-19 22:56:46 -0500

    feat(src/reducers/index.js): add header reducer to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
+import header from './header';
 
 const rootReducer = combineReducers({
+  header,
   routing: routerReducer,
 });
 
