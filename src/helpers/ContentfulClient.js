720b7cc39527e65269603530fac546f5566333d0
Antonios Karagiannis <antokarag@gmail.com>
2017-12-26 19:39:05 -0500

    feat(src/helpers/ContentfulClient.js): add ContentfulClient helper

---
+++ src/helpers/ContentfulClient.js
@@ -0,0 +1,23 @@
+import { createClient } from 'contentful';
+import contentfulEntries from 'Constants/contentfulEntries';
+
+class ContentfulClient {
+  constructor() {
+    this.pClient = createClient({
+      space: process.env.CONTENTFUL_SPACE,
+      accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+    });
+    this.pEntries = contentfulEntries;
+  }
+  get client() {
+    return this.pClient;
+  }
+  get entries() {
+    return this.pEntries;
+  }
+  getEntry(id, query = undefined) {
+    return this.pClient.getEntry(id, query);
+  }
+}
+
+export default ContentfulClient;
