f90e2b8ec103ab7b24ca493cd2db9ce2b632f07d
Antonios Karagiannis <antokarag@gmail.com>
2017-12-28 22:15:36 -0500

    feat(src/actions/getHeader.js): change to use redux-actions and redux-promise

--- src/actions/getHeader.js
+++ src/actions/getHeader.js
@@ -1,24 +1,10 @@
 import {
   GET_HEADER,
-  GET_HEADER_SUCCESS,
-  GET_HEADER_FAILURE,
 } from 'Constants/actionTypes';
+import { createAction } from 'redux-actions';
+import ContentfulClient from 'Helpers/ContentfulClient';
 
-export const getHeader = cc => (
-  {
-    type: GET_HEADER,
-    request: cc.getEntry(cc.entries.header),
-  }
-);
-export const getHeaderSuccess = fields => (
-  {
-    type: GET_HEADER_SUCCESS,
-    fields,
-  }
-);
-export const getHeaderFailure = error => (
-  {
-    type: GET_HEADER_FAILURE,
-    error,
-  }
-);
+const cc = new ContentfulClient();
+const getHeader = createAction(GET_HEADER, async () => cc.getEntry(cc.entries.header));
+
+export default getHeader;
