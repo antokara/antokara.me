16d95660381684d3155a77a0d51606287c03a967
Antonios Karagiannis <antokarag@gmail.com>
2017-12-30 22:25:38 -0500

    feat(src/components/Header.jsx): add HeaderIcons, default props and refactor for spread props

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import HeaderIcons from './HeaderIcons';
 import style from './Header.pcss';
 
 class Header extends React.Component {
@@ -13,16 +14,16 @@ class Header extends React.Component {
       <header className={style.header}>
         <div className={style.text}>
           <div className={style.upper}>
-            <div>{this.props.fields.first}</div>
-            <div>{this.props.fields.second}</div>
+            <div>{this.props.first}</div>
+            <div>{this.props.second}</div>
           </div>
           <div className={style.lower}>
-            <div>{this.props.fields.third}</div>
-            <div>{this.props.fields.fourth}</div>
+            <div>{this.props.third}</div>
+            <div>{this.props.fourth}</div>
           </div>
         </div>
         <div className={style.icons}>
-          X Y Z
+          <HeaderIcons icons={this.props.icons} />
         </div>
       </header>
     );
@@ -31,12 +32,25 @@ class Header extends React.Component {
 
 Header.propTypes = {
   getHeader: PropTypes.func.isRequired,
-  fields: PropTypes.shape({
-    first: PropTypes.string,
-    second: PropTypes.string,
-    third: PropTypes.string,
-    fourth: PropTypes.string,
-  }).isRequired,
+  first: PropTypes.string,
+  second: PropTypes.string,
+  third: PropTypes.string,
+  fourth: PropTypes.string,
+  icons: PropTypes.arrayOf(PropTypes.shape({
+    url: PropTypes.string,
+    alt: PropTypes.string,
+    assetUrl: PropTypes.string,
+    internal: PropTypes.bool,
+  })),
 };
+
+Header.defaultProps = {
+  first: '',
+  second: '',
+  third: '',
+  fourth: '',
+  icons: [],
+};
+
 
 export default Header;
