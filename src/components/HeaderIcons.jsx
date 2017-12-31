e3a3ab493c04257e4986b3a9638b5fb4bc4c9905
Antonios Karagiannis <antokarag@gmail.com>
2017-12-30 22:24:10 -0500

    feat(src/components/HeaderIcons): add HeaderIcons component and its stylesheet

---
+++ src/components/HeaderIcons.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import routes from 'Constants/routes';
+import style from './HeaderIcons.pcss';
+
+const HeaderIcons = (props) => {
+  if (!props.icons.length) {
+    return false;
+  }
+
+  const icons = props.icons.map((icon) => {
+    const img = <img src={icon.assetUrl} alt={icon.alt} />;
+    if (icon.internal) {
+      return <Link key={icon.alt} to={routes[icon.url]} title={icon.alt}>{img}</Link>;
+    }
+    return <a key={icon.alt} href={icon.url} title={icon.alt} target="_blank">{img}</a>;
+  });
+
+  return (
+    <div className={style.headerIcons}>
+      {icons}
+    </div>
+  );
+};
+
+HeaderIcons.propTypes = {
+  icons: PropTypes.arrayOf(PropTypes.shape({
+    url: PropTypes.string,
+    alt: PropTypes.string,
+    assetUrl: PropTypes.string,
+    internal: PropTypes.bool,
+  })).isRequired,
+};
+
+export default HeaderIcons;
