3cc1716bf6b1c7caa89338b6bf168e21a2bcb215
Antonios Karagiannis <antokarag@gmail.com>
2017-12-31 21:49:50 -0500

    feat(src/components/SVG.jsx): add SVG component

---
+++ src/components/SVG.jsx
@@ -0,0 +1,156 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+// import style from './About.pcss';
+
+const parseXML = (text, options = null) => {
+  const defaultOptions = {
+    wrapper: 'div',
+    safeIds: [],
+    padding: 0,
+    aspect: {
+      width: null,
+      height: null,
+    },
+    max: {
+      width: null,
+      height: null,
+    },
+  };
+  const opts = { ...defaultOptions, options };
+  const oDOM = new DOMParser().parseFromString(text, 'image/svg+xml');
+  if (oDOM.documentElement.nodeName === 'parsererror' || oDOM.documentElement.nodeName !== 'svg') {
+    return false;
+  }
+
+  // check if there are width, height attributes.
+  // if true, keep their values but remove them
+  let width = oDOM.documentElement.getAttribute('width');
+  let height = oDOM.documentElement.getAttribute('height');
+  if (width !== null) {
+    oDOM.documentElement.removeAttribute('width');
+  }
+  if (height !== null) {
+    oDOM.documentElement.removeAttribute('height');
+  }
+
+  // get the viewBox and get its individual values
+  let viewBox = oDOM.documentElement.getAttribute('viewBox');
+  if (viewBox !== null) {
+    viewBox = viewBox.split(' ');
+    if (viewBox.length !== 4) {
+      viewBox = viewBox.split(',');
+    }
+    if (viewBox.length === 4) {
+      viewBox = {
+        minX: viewBox[0],
+        minY: viewBox[1],
+        width: viewBox[2],
+        height: viewBox[3],
+      };
+    } else {
+      // if we couldn't retrieve the viewBox values, reset it
+      viewBox = null;
+    }
+  }
+  if (opts.aspect.width === null) {
+    // if no width attribute was found but we have the viewBox
+    if (width === null && viewBox !== null) {
+      [width] = viewBox;
+    }
+    opts.aspect.width = width;
+  }
+  if (opts.aspect.height === null) {
+    opts.aspect.height = height;
+    if (height === null && viewBox !== null) {
+      [height] = viewBox;
+    }
+    opts.aspect.height = height;
+  }
+
+  // in case no aspect width and height was found or given
+  if (opts.aspect.width === null || opts.aspect.height === null) {
+    throw new Error('No aspect.width and aspect.height for the SVG was found or given');
+  }
+
+  // if max dimensions are set to default, use the aspect
+  if (opts.max.width === null) {
+    opts.max.width = opts.aspect.width;
+  }
+  if (opts.max.height === null) {
+    opts.max.height = opts.aspect.height;
+  }
+
+  // check and modify element IDs, to avoid warnings/errors when the same SVG
+  // is used multiple times in the same page (avoid duplicate element IDs)
+  // @todo
+
+  // in case no viewBox was found
+  if (viewBox === null) {
+    viewBox = {
+      minX: 0,
+      minY: 0,
+      width: opts.aspect.width,
+      height: opts.aspect.height,
+    };
+  }
+
+  // in case padding is needed
+  if (opts.padding) {
+    // adjust viewBox for padding
+    viewBox.minX -= opts.padding;
+    viewBox.minY -= opts.padding;
+    viewBox.width += opts.padding;
+    viewBox.height += opts.padding;
+  }
+
+  // set the new viewBox
+  oDOM.documentElement.setAttribute(
+    'viewBox',
+    `${viewBox.minX} ${viewBox.minY} ${viewBox.width} ${viewBox.height}`,
+  );
+
+  // create the canvas and context for our aspect holder
+  const canvas = document.createElement('canvas');
+  canvas.width = opts.aspect.width;
+  canvas.height = opts.aspect.height;
+
+  // create the aspect holder
+  const aspectHolder = new Document().createElement('img');
+  aspectHolder.setAttribute('class', 'svgInlinerAspect');
+  aspectHolder.setAttribute('src', canvas.toDataURL('image/png', 0));
+
+  const serializer = new XMLSerializer();
+  return serializer.serializeToString(aspectHolder) +
+    serializer.serializeToString(oDOM.documentElement);
+};
+
+class SVG extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      svg: { __html: '' },
+    };
+    fetch(props.url)
+      .then((response) => {
+        response.text().then((text) => {
+          this.setState({ svg: { __html: parseXML(text) } });
+        });
+      }).catch(() => {
+        this.setState({ svg: false });
+      });
+  }
+
+  render() {
+    // remove url prop
+    const attrs = { ...this.props };
+    delete attrs.url;
+    // eslint-disable-next-line react/no-danger
+    return <div className="svgInlinerWrap" dangerouslySetInnerHTML={this.state.svg} {...attrs} />;
+  }
+}
+
+SVG.propTypes = {
+  url: PropTypes.string.isRequired,
+};
+
+export default SVG;
