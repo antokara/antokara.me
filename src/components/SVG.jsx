8ccfa45a76e1f454fd9bf8ee61762f32503e3e47
Antonios Karagiannis <antokarag@gmail.com>
2017-12-31 23:31:58 -0500

    fix(src/components/SVG.jsx): fix defaultOptions and rename options.aspect props

--- src/components/SVG.jsx
+++ src/components/SVG.jsx
@@ -3,7 +3,20 @@ import PropTypes from 'prop-types';
 import style from './SVG.pcss';
 
 const parseXML = (text, options = null) => {
-  const opts = { ...options };
+  const defaultOptions = {
+    wrapper: 'div',
+    safeIds: [],
+    padding: 0,
+    aspect: {
+      x: null,
+      y: null,
+    },
+    max: {
+      width: 0,
+      height: 0,
+    },
+  };
+  const opts = { ...defaultOptions, ...options };
   const oDOM = new DOMParser().parseFromString(text, 'image/svg+xml');
   if (oDOM.documentElement.nodeName === 'parsererror' || oDOM.documentElement.nodeName !== 'svg') {
     return false;
@@ -39,32 +52,32 @@ const parseXML = (text, options = null) => {
       viewBox = null;
     }
   }
-  if (opts.aspect.width === null) {
+  if (opts.aspect.x === null) {
     // if no width attribute was found but we have the viewBox
     if (width === null && viewBox !== null) {
       [width] = viewBox;
     }
-    opts.aspect.width = width;
+    opts.aspect.x = width;
   }
-  if (opts.aspect.height === null) {
-    opts.aspect.height = height;
+  if (opts.aspect.y === null) {
+    opts.aspect.y = height;
     if (height === null && viewBox !== null) {
       [height] = viewBox;
     }
-    opts.aspect.height = height;
+    opts.aspect.y = height;
   }
 
   // in case no aspect width and height was found or given
-  if (opts.aspect.width === null || opts.aspect.height === null) {
-    throw new Error('No aspect.width and aspect.height for the SVG was found or given');
+  if (opts.aspect.x === null || opts.aspect.y === null) {
+    throw new Error('No aspect.x and aspect.y for the SVG was found or given');
   }
 
   // if max dimensions are set to null, use the aspect
   if (opts.max.width === null) {
-    opts.max.width = `${opts.aspect.width}px`;
+    opts.max.width = `${opts.aspect.x}px`;
   }
   if (opts.max.height === null) {
-    opts.max.height = `${opts.aspect.height}px`;
+    opts.max.height = `${opts.aspect.y}px`;
   }
 
   // check and modify element IDs, to avoid warnings/errors when the same SVG
@@ -76,8 +89,8 @@ const parseXML = (text, options = null) => {
     viewBox = {
       minX: 0,
       minY: 0,
-      width: opts.aspect.width,
-      height: opts.aspect.height,
+      width: opts.aspect.x,
+      height: opts.aspect.y,
     };
   }
 
@@ -100,8 +113,8 @@ const parseXML = (text, options = null) => {
 
   // create the canvas and context for our aspect holder
   const canvas = document.createElement('canvas');
-  canvas.width = opts.aspect.width;
-  canvas.height = opts.aspect.height;
+  canvas.width = opts.aspect.x;
+  canvas.height = opts.aspect.y;
 
   // create the aspect holder
   const aspectHolder = document.createElement('img');
@@ -149,34 +162,23 @@ class SVG extends React.Component {
 
 SVG.propTypes = {
   url: PropTypes.string.isRequired,
-  options: PropTypes.objectOf(PropTypes.shape({
+  options: PropTypes.shape({
     wrapper: PropTypes.string,
     safeIds: PropTypes.arrayOf(PropTypes.string),
     padding: PropTypes.number,
-    aspect: PropTypes.objectOf(PropTypes.shape({
-      width: PropTypes.number,
-      height: PropTypes.number,
-    })),
-    max: PropTypes.objectOf(PropTypes.shape({
-      width: PropTypes.number,
-      height: PropTypes.number,
-    })),
-  })),
+    aspect: PropTypes.shape({
+      x: PropTypes.number,
+      y: PropTypes.number,
+    }),
+    max: PropTypes.shape({
+      width: PropTypes.string,
+      height: PropTypes.string,
+    }),
+  }),
 };
 
 SVG.defaultProps = {
   options: {
-    wrapper: 'div',
-    safeIds: [],
-    padding: 0,
-    aspect: {
-      width: null,
-      height: null,
-    },
-    max: {
-      width: 0,
-      height: 0,
-    },
   },
 };
 
