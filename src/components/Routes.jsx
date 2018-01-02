42cf16d4949b53f9d5622e6a57108f2781c4c37b
Antonios Karagiannis <antokarag@gmail.com>
2018-01-01 20:54:44 -0500

    feat(src/components/Routes.jsx): add new routes

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Route } from 'react-router';
 import Home from 'Components/Home.jsx';
-import About from 'Components/About.jsx';
+import Skills from 'Components/Skills.jsx';
+import Demos from 'Components/Demos.jsx';
+import Contact from 'Components/Contact.jsx';
 import routes from 'Constants/routes';
 
 const Routes = () => (
   <div>
     <Route exact path={routes.home} component={Home} />
-    <Route path={routes.about} component={About} />
+    <Route exact path={routes.skills} component={Skills} />
+    <Route exact path={routes.demos} component={Demos} />
+    <Route exact path={routes.contact} component={Contact} />
   </div>
 );
 
