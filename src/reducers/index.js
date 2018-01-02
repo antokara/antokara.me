bd8368041f24049eb0541aa4785fa2c4a56df5f2
Antonios Karagiannis <antokarag@gmail.com>
2018-01-02 11:58:18 -0500

    feat(src/reducers/index.js): add mainMenu reducer to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import header from './header';
+import mainMenu from './mainMenu';
 
 const rootReducer = combineReducers({
   header,
+  mainMenu,
   routing: routerReducer,
 });
 
