645e26875a10d2d509c9ed648c6991342792105c
Antonios Karagiannis <antokarag@gmail.com>
2018-01-01 20:53:16 -0500

    feat(src/components/HeaderIcons.jsx): add error handler

--- src/components/HeaderIcons.jsx
+++ src/components/HeaderIcons.jsx
@@ -5,25 +5,41 @@ import routes from 'Constants/routes';
 import SVG from 'Components/SVG.jsx';
 import style from './HeaderIcons.pcss';
 
-const HeaderIcons = (props) => {
-  if (!props.icons.length) {
-    return false;
+class HeaderIcons extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+    this.props = props;
   }
 
-  const icons = props.icons.map((icon) => {
-    const img = <SVG url={icon.assetUrl} />;
-    if (icon.internal) {
-      return <Link key={icon.alt} to={routes[icon.url]} title={icon.alt}>{img}</Link>;
-    }
-    return <a key={icon.alt} href={icon.url} title={icon.alt} target="_blank">{img}</a>;
-  });
+  componentDidCatch() {
+    this.setState({ hasError: true });
+  }
 
-  return (
-    <div className={style.headerIcons}>
-      {icons}
-    </div>
-  );
-};
+  render() {
+    if (this.state.hasError) {
+      return <div>[error]</div>;
+    }
+
+    if (!this.props.icons.length) {
+      return false;
+    }
+
+    const icons = this.props.icons.map((icon) => {
+      const img = <SVG url={icon.assetUrl} />;
+      if (icon.internal) {
+        return <Link key={icon.alt} to={routes[icon.url]} title={icon.alt}>{img}</Link>;
+      }
+      return <a key={icon.alt} href={icon.url} title={icon.alt} target="_blank">{img}</a>;
+    });
+
+    return (
+      <div className={style.headerIcons}>
+        {icons}
+      </div>
+    );
+  }
+}
 
 HeaderIcons.propTypes = {
   icons: PropTypes.arrayOf(PropTypes.shape({
