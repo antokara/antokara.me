8b37d823a6eddec7ba980ad3335a3370e905872e
Antonios Karagiannis <antokarag@gmail.com>
2018-01-04 22:02:08 -0500

    feat(src/reducers/index.js): add home reducer to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import header from './header';
 import mainMenu from './mainMenu';
+import home from './home';
 
 const rootReducer = combineReducers({
   header,
   mainMenu,
+  home,
   routing: routerReducer,
 });
 
