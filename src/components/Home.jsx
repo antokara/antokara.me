82c62288c7731336dc910eeb7bdea625b8487e27
Antonios Karagiannis <antokarag@gmail.com>
2018-01-07 22:02:45 -0500

    feat(src/components/Home.jsx): make SVG responsive

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,28 +11,32 @@ class Home extends React.Component {
   }
 
   componentDidUpdate() {
-    this.draw = SVG(style.home);
-    const cX = 60;
-    const cY = 60;
-    const cDiameter = 100;
+    const vW = 200;
+    const vH = 175;
+    this.draw = SVG(style.home)
+      .size(vW, vH).attr('width', null).attr('height', null)
+      .viewbox(0, 0, vW, vH);
+    const cX = vW / 4;
+    const cY = vH / 3;
+    const cDiameter = vH / 3;
     const rect = this.draw.circle(cDiameter)
       .attr({ fill: '#000', opacity: 0.12 })
-      .stroke({ width: 4, color: '#fff', opacity: 1 })
-      .center(cY, cX);
+      .stroke({ width: 3, color: '#fff', opacity: 1 })
+      .center(cX, cY);
     const first = this.draw.text(this.props.first).font({
       family: 'Titillium Web',
-      size: 40,
+      size: vW / 10,
       weight: 'bold',
     }).center(cX, cY);
     const second = this.draw.text(this.props.second).font({
       family: 'Titillium Web',
-      size: 40,
+      size: vW / 10,
       weight: 'bold',
     }).x(cX + (cDiameter / 1.75)).cy(cY);
     const third = this.draw.text(this.props.third).font({
       family: 'Titillium Web',
-      size: 28,
-    }).x(cX + (cDiameter * 1.5)).y(cY * 1.35);
+      size: vW / 15,
+    }).x(cX + cDiameter).y(cY * 1.35);
   }
 
   componentWillUnmount() {
@@ -41,17 +45,7 @@ class Home extends React.Component {
 
   render() {
     return (
-      <div id={style.home} className={style.home}>
-        <div>
-          {this.props.first}
-        </div>
-        <div>
-          {this.props.second}
-        </div>
-        <div>
-          {this.props.third} {this.props.rotator}
-        </div>
-      </div>
+      <div id={style.home} className={style.home} />
     );
   }
 }
