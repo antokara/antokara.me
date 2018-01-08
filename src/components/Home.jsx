b2e6cccbf19a049d795c89404d0624a237e04652
Antonios Karagiannis <antokarag@gmail.com>
2018-01-07 22:19:32 -0500

    feat(src/components/Home.jsx): add animation to the SVG

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,32 +11,66 @@ class Home extends React.Component {
   }
 
   componentDidUpdate() {
+    // viewport
     const vW = 200;
     const vH = 175;
+
+    // svg draw
     this.draw = SVG(style.home)
       .size(vW, vH).attr('width', null).attr('height', null)
       .viewbox(0, 0, vW, vH);
+
+    // center X,Y for circle (main ref point)
     const cX = vW / 4;
     const cY = vH / 3;
+    // circle diameter
     const cDiameter = vH / 3;
-    const rect = this.draw.circle(cDiameter)
-      .attr({ fill: '#000', opacity: 0.12 })
+
+    this.draw.circle(1)
+      .attr({ fill: '#000', opacity: 0 })
       .stroke({ width: 3, color: '#fff', opacity: 1 })
-      .center(cX, cY);
-    const first = this.draw.text(this.props.first).font({
-      family: 'Titillium Web',
-      size: vW / 10,
-      weight: 'bold',
-    }).center(cX, cY);
-    const second = this.draw.text(this.props.second).font({
-      family: 'Titillium Web',
-      size: vW / 10,
-      weight: 'bold',
-    }).x(cX + (cDiameter / 1.75)).cy(cY);
-    const third = this.draw.text(this.props.third).font({
-      family: 'Titillium Web',
-      size: vW / 15,
-    }).x(cX + cDiameter).y(cY * 1.35);
+      .center(cX, cY)
+      .animate(850, '>', 500)
+      .attr({ opacity: 0.12 })
+      .radius(cDiameter / 2);
+
+    const first = this.draw.text(this.props.first)
+      .font({
+        family: 'Titillium Web',
+        size: vW / 10,
+        weight: 'bold',
+      })
+      .cy(cY)
+      .attr({ opacity: 0 });
+    first.cx(cX - first.bbox().width)
+      .animate(1000, '>', 500)
+      .cx(cX)
+      .attr({ opacity: 1 });
+
+    const second = this.draw.text(this.props.second)
+      .font({
+        family: 'Titillium Web',
+        size: vW / 10,
+        weight: 'bold',
+      })
+      .cy(cY)
+      .attr({ opacity: 0 });
+    second.x((cX + (cDiameter / 1.75)) - second.bbox().width)
+      .animate(1000, '>', 500)
+      .x(cX + (cDiameter / 1.75))
+      .attr({ opacity: 1 });
+
+    const third = this.draw.text(this.props.third)
+      .font({
+        family: 'Titillium Web',
+        size: vW / 15,
+      })
+      .y(cY * 1.35)
+      .attr({ opacity: 0 });
+    third.x((cX + cDiameter) - third.bbox().width)
+      .animate(900, '>', 700)
+      .x(cX + cDiameter)
+      .attr({ opacity: 1 });
   }
 
   componentWillUnmount() {
