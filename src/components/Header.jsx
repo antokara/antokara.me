b0211ebee33b0959262a7f8cfdb55f5f2de86aa6
Antonios Karagiannis <antokarag@gmail.com>
2018-01-09 21:42:27 -0500

    refactor(src/components/Header.jsx): change to use props without get action

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,35 +3,25 @@ import PropTypes from 'prop-types';
 import IconLinks from './IconLinks';
 import style from './Header.pcss';
 
-class Header extends React.Component {
-  constructor(props) {
-    super(props);
-    props.getHeader();
-  }
-
-  render() {
-    return (
-      <header className={style.header}>
-        <div className={style.text}>
-          <div className={style.upper}>
-            <div>{this.props.first}</div>
-            <div>{this.props.second}</div>
-          </div>
-          <div className={style.lower}>
-            <div>{this.props.third}</div>
-            <div>{this.props.fourth}</div>
-          </div>
-        </div>
-        <div className={style.icons}>
-          <IconLinks icons={this.props.icons} />
-        </div>
-      </header>
-    );
-  }
-}
+const Header = props => (
+  <header className={style.header}>
+    <div className={style.text}>
+      <div className={style.upper}>
+        <div>{props.first}</div>
+        <div>{props.second}</div>
+      </div>
+      <div className={style.lower}>
+        <div>{props.third}</div>
+        <div>{props.fourth}</div>
+      </div>
+    </div>
+    <div className={style.icons}>
+      <IconLinks icons={props.icons} />
+    </div>
+  </header>
+);
 
 Header.propTypes = {
-  getHeader: PropTypes.func.isRequired,
   first: PropTypes.string,
   second: PropTypes.string,
   third: PropTypes.string,
