89a4523b2e79e227b1217a7613bd114d7be1c46d
Antonios Karagiannis <antokarag@gmail.com>
2018-01-09 21:41:16 -0500

    feat(src/actions/getTheme.js): add getTheme action

---
+++ src/actions/getTheme.js
@@ -0,0 +1,13 @@
+import {
+  GET_THEME,
+} from 'Constants/actionTypes';
+import { createAction } from 'redux-actions';
+import ContentfulClient from 'Helpers/ContentfulClient';
+
+const cc = new ContentfulClient();
+const getTheme = createAction(GET_THEME, async () => cc.getEntries({
+  'sys.id': cc.entries.theme,
+  include: 2,
+}));
+
+export default getTheme;
