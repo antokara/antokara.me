4a2e575fc3de7824fc8a076b5d9a8f5de39514c2
Antonios Karagiannis <antokarag@gmail.com>
2018-01-12 22:04:48 -0500

    fix(src/index.jsx): fix css order for css normalize
    change to be first stylesheet imported, so that any following css properly overrides it

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
+import 'normalize.css';
 import React from 'react';
 import { render } from 'react-dom';
 import Root from 'Containers/Root.jsx';
-import 'normalize.css';
 import './index.pcss';
 
 render(
