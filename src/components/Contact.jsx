24948b06ab59cab2e6d5f8eb71dc2e3690ca0a6b
Antonios Karagiannis <antokarag@gmail.com>
2018-01-14 21:40:51 -0500

    feat(src/components/Contact.jsx): add IconLink usage for the icons

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,48 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import IconLink from './IconLink';
 import style from './Contact.pcss';
 
-const Contact = () => (
-  <div className={style.contact}>
-    Contact
-  </div>
-);
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    props.getContact();
+    this.props = props;
+  }
+
+  render() {
+    const icons = this.props.icons.map(icon => (
+      <div key={icon.alt}><IconLink {...icon} /></div>
+    ));
+    return (
+      <div className={style.contact}>
+        <div className={style.icons}>
+          {icons}
+        </div>
+        <div className={style.text}>
+          {this.props.text[0]}<br />
+          {this.props.text[1]}, {this.props.text[2]}
+        </div>
+      </div>
+    );
+  }
+}
+
+Contact.propTypes = {
+  getContact: PropTypes.func.isRequired,
+  text: PropTypes.arrayOf(PropTypes.string),
+  icons: PropTypes.arrayOf(PropTypes.shape({
+    url: PropTypes.string,
+    alt: PropTypes.string,
+    assetUrl: PropTypes.string,
+    internal: PropTypes.bool,
+  })),
+};
+
+
+Contact.defaultProps = {
+  text: [],
+  icons: [],
+};
 
 export default Contact;
