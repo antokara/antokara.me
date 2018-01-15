c64b127508721d2a931597d606838d96ac5a89e0
Antonios Karagiannis <antokarag@gmail.com>
2018-01-14 21:39:07 -0500

    feat(src/reducers/index.js): add contact reducer to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import * as theme from './theme';
 import home from './home';
+import contact from './contact';
 
 const rootReducer = combineReducers({
   theme: theme.reducer,
   routing: routerReducer,
   home,
+  contact,
 });
 
 export default rootReducer;
