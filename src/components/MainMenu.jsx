3898e20ce27e1e6cde28e6a1a4ba44d327872c45
Antonios Karagiannis <antokarag@gmail.com>
2018-01-18 23:09:24 -0500

    feat(src/components/MainMenu.jsx): enhance animation
    * add circle prop
    * convert to React.Component
    * add className to IconLinks
    * add marker circle

--- src/components/MainMenu.jsx
+++ src/components/MainMenu.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import SVG from './SVG';
 import IconLinks from './IconLinks';
 import style from './MainMenu.pcss';
 
-const MainMenu = props => (
-  <div className={style.mainMenu}>
-    <IconLinks icons={props.icons} activeClassName={style.active} />
-  </div>
-);
+
+class MainMenu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+  }
+
+  render() {
+    return (
+      <div className={style.mainMenu}>
+        <IconLinks
+          icons={this.props.icons}
+          className={style.menuItem}
+          activeClassName={style.active}
+        />
+        <SVG className={style.marker} url={this.props.circle} />
+      </div>
+    );
+  }
+}
 
 MainMenu.propTypes = {
   icons: PropTypes.arrayOf(PropTypes.shape({
@@ -16,10 +32,12 @@ MainMenu.propTypes = {
     assetUrl: PropTypes.string,
     internal: PropTypes.bool,
   })),
+  circle: PropTypes.string,
 };
 
 MainMenu.defaultProps = {
   icons: [],
+  circle: '',
 };
 
 
