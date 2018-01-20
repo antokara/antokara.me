1cb8693cf46e0534854dc0b88d91943be6f6bd16
Antonios Karagiannis <antokarag@gmail.com>
2018-01-19 22:24:22 -0500

    test(src/components/Contact.test.jsx): update stub test to pass

--- src/components/Contact.test.jsx
+++ src/components/Contact.test.jsx
@@ -4,13 +4,8 @@ import Contact from './Contact';
 import style from './Contact.pcss';
 
 describe('Contact component', () => {
-  const c = shallow(<Contact />);
-
+  const c = shallow(<Contact getContact={() => {}} />);
   test('has the correct css class applied to it', () => {
     expect(c.hasClass(style.contact)).toBe(true);
-  });
-
-  test('inner div has the correct content', () => {
-    expect(c.find('div[data-id="inner"]').text()).toBe('Contact');
   });
 });
