1f73fbbd30a683fe7064f4c1c71a72775cbe3d96
Antonios Karagiannis <antokarag@gmail.com>
2018-01-26 23:23:28 -0500

    feat(src/actions/getSkills.js): add getSkills action

---
+++ src/actions/getSkills.js
@@ -0,0 +1,13 @@
+import {
+  GET_SKILLS,
+} from 'Constants/actionTypes';
+import { createAction } from 'redux-actions';
+import ContentfulClient from 'Helpers/ContentfulClient';
+
+const cc = new ContentfulClient();
+const getSkills = createAction(GET_SKILLS, async () => cc.getEntries({
+  'sys.id': cc.entries.skills,
+  include: 5,
+}));
+
+export default getSkills;
