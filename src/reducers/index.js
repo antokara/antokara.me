38eadb10f0fd287740d06f0d0f3b6a9a2daede1a
Antonios Karagiannis <antokarag@gmail.com>
2018-01-26 23:26:32 -0500

    feat(src/reducers/index.js): add skills reducer to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,12 +2,14 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import * as theme from './theme';
 import home from './home';
+import skills from './skills';
 import contact from './contact';
 
 const rootReducer = combineReducers({
   theme: theme.reducer,
   routing: routerReducer,
   home,
+  skills,
   contact,
 });
 
