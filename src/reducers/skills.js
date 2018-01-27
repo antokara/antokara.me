58ded6f0a467968c07ec3756f6d8649e107f490d
Antonios Karagiannis <antokarag@gmail.com>
2018-01-26 23:26:19 -0500

    feat(src/reducers/skills.js): add skills reducer

---
+++ src/reducers/skills.js
@@ -0,0 +1,54 @@
+import {
+  GET_SKILLS,
+} from 'Constants/actionTypes';
+import { handleAction } from 'redux-actions';
+
+const defaultState = {
+  nodes: [],
+  edges: [],
+};
+
+/**
+ * generates the network of nodes and edges,
+ * ready to be used in vis.network to avoid unessesary
+ * extra parsing/iteration inside the component
+ *
+ * @param {integer} parentId id of the parent node
+ * @param {integer} level zero based level of node in hierarchy
+ * @param {object} field the field retrieved from contentful
+ * @param {array} nodes the array of nodes
+ * @param {array} edges the array of edges
+ */
+const generateNetwork = (parentId, level, field, nodes, edges) => {
+  // add this skill
+  const id = nodes.length + 1;
+  nodes.push({
+    id,
+    label: field.name,
+    level,
+    hidden: level > 1,
+  });
+  if (parentId) {
+    edges.push({
+      from: parentId,
+      to: id,
+    });
+  }
+  // add children skills
+  if (field.skills) {
+    field.skills.forEach(element => generateNetwork(id, level + 1, element.fields, nodes, edges));
+  }
+};
+
+const skills = handleAction(GET_SKILLS, (state, action) => {
+  const nodes = [];
+  const edges = [];
+  generateNetwork(null, 0, action.payload.items[0].fields.skill.fields, nodes, edges);
+  return {
+    ...state,
+    nodes,
+    edges,
+  };
+}, defaultState);
+
+export default skills;
