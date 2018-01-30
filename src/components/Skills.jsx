f25e4ae6a0082efe375f87ad07c11b8a794d2059
Antonios Karagiannis <antokarag@gmail.com>
2018-01-29 21:33:00 -0500

    feat(src/components/Skills.jsx): test d3 force simulation on simple network

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -30,32 +30,101 @@ class Skills extends React.Component {
       // });
       const width = 960;
       const height = 500;
-      const data = [
+      // const data = [
+      //   {
+      //     label: 'skills',
+      //     expanded: true,
+      //     children: [
+      //       { label: 'frontend' },
+      //       { label: 'backend' },
+      //     ],
+      //   },
+      // ];
+
+      // const svg = d3
+      //   .select(`div.${style.skills}`)
+      //   .append('svg')
+      //   .attr('width', width)
+      //   .attr('height', height)
+      //   .selectAll('circle')
+      //   .data(data)
+      //   .enter()
+      //   .append('circle')
+      //   .style('r', d => d.label.length * 5)
+      //   .style('cx', '50')
+      //   .style('cy', '50')
+      //   .style('fill', 'white')
+      //   .style('stroke', 'black')
+      //   .text(d => d.label);
+
+      const nodes = [
         {
+          id: 0,
           label: 'skills',
-          expanded: true,
-          children: [
-            { label: 'frontend' },
-            { label: 'backend' },
-          ],
+        },
+        {
+          id: 1,
+          label: 'frontend',
+        },
+        {
+          id: 2,
+          label: 'backend',
         },
       ];
-
+      const links = [
+        {
+          from: 0,
+          to: 1,
+          source: nodes[0],
+          target: nodes[1],
+        },
+        {
+          from: 0,
+          to: 2,
+          source: nodes[0],
+          target: nodes[2],
+        },
+      ];
       const svg = d3
         .select(`div.${style.skills}`)
         .append('svg')
         .attr('width', width)
-        .attr('height', height)
+        .attr('height', height);
+
+      const link = svg.append('g')
+        .attr('class', 'links')
+        .selectAll('line')
+        .data(links)
+        .enter()
+        .append('line');
+
+      const node = svg.append('g')
+        .attr('class', 'nodes')
         .selectAll('circle')
-        .data(data)
+        .data(nodes)
         .enter()
         .append('circle')
-        .style('r', d => d.label.length * 5)
-        .style('cx', '50')
-        .style('cy', '50')
-        .style('fill', 'white')
-        .style('stroke', 'black')
-        .text(d => d.label);
+        .attr('r', 40);
+
+      node.append('title').text(d => (d.label));
+
+      const simulation = d3.forceSimulation()
+        .force('link', d3.forceLink().id(d => (d.id)))
+        .force('charge', d3.forceManyBody().strength(-1000))
+        .force('center', d3.forceCenter(width / 2, height / 2));
+
+      simulation.nodes(nodes).on('tick', () => {
+        node
+          .attr('cx', d => (d.x))
+          .attr('cy', d => (d.y));
+        link
+          .attr('x1', d => (d.source.x))
+          .attr('y1', d => (d.source.y))
+          .attr('x2', d => (d.target.x))
+          .attr('y2', d => (d.target.y));
+      });
+
+      simulation.force('link').links(links);
     }
 
     return (
