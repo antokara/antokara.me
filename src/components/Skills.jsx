7172de72128cead653c4598f92ca3cc6939d28b8
Antonios Karagiannis <antokarag@gmail.com>
2018-02-03 21:58:00 -0500

    feat(src/components/Skills.jsx): add collisionRadiusModifier in the simulation
    spacing between nodes is now proportional

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -48,9 +48,6 @@ class Skills extends React.Component {
       //   background: '#99c5eb',
       // },
 
-      const width = 1960;
-      const height = 500;
-
       // const svg = d3
       //   .select(`div.${style.skills}`)
       //   .append('svg')
@@ -67,30 +64,55 @@ class Skills extends React.Component {
       //   .style('stroke', 'black')
       //   .text(d => d.label);
 
-      const svg = d3
-        .select(`div.${style.skills}`)
-        .append('svg')
-        .attr('width', width)
-        .attr('height', height);
+      // dimensions of our SVG
+      const width = 1960;
+      const height = 500;
 
-      // create the links/edges as the lowest z-index
-      const link = svg.append('g')
-        .attr('class', 'links')
-        .selectAll('line')
-        .data(links)
-        .enter()
-        .append('line');
+      // the larger the number, the less space will be available
+      // between the text and the edge of the node circle
+      const radiusModifier = 0.8;
 
-      const modifier = 0.8;
+      // maximum radius for each node circle
+      // when the text is greater than that
+      // the font-size will shrink to fit that
       const maxRadius = 50;
+
+      // maximum text length allowed
+      // when the text is greater than that
+      // the font-size will shrink to fit that
       const maxLength = maxRadius / 5;
+
+      // the multiplier to apply on the collision radius
+      // if equal to 1 then the space between each node can be zero
+      // by increasing this number, we guarantee the spacing between each node and
+      // since it is a multiplier applied to the radius, it will apply proportionally...
+      const collisionRadiusModifier = 1.2;
+
+      /**
+       * calculates the radius of each node
+       * using the textWidth provided and
+       * imposing a maximum radius to the result
+       *
+       * @param {float} textWidth label text width as provided by the getComputedTextLength()
+       * @return {float} radius to be applied on the node's circle
+       */
       const radius = (textWidth) => {
-        if (textWidth > maxRadius) {
-          return maxRadius;
+        let rad = textWidth * radiusModifier;
+        if (rad > maxRadius) {
+          rad = maxRadius;
         }
-        return textWidth * modifier;
+        return rad;
       };
 
+      /**
+       * calculates the font size to apply to the node's label
+       * imposing a maximum length to the result.
+       * When a label is over the limit, the font size gets shranked
+       * to make it fit the node's circle radius
+       *
+       * @param {integer} length label text characters length of the node
+       * @return {float} font-size in 'em' unit, to apply to the node's label
+       */
       const fontSize = (length) => {
         if (length > maxLength) {
           return maxLength / length;
@@ -98,9 +120,25 @@ class Skills extends React.Component {
         return 1;
       };
 
+      // create the SVG which will contain our network
+      const svg = d3
+        .select(`div.${style.skills}`)
+        .append('svg')
+        .attr('width', width)
+        .attr('height', height);
+
+      // create the links/edges as the bottom z-index
+      const link = svg.append('g')
+        .attr('class', 'links')
+        .selectAll('line')
+        .data(links)
+        .enter()
+        .append('line');
+
       // create the nodes group as the mid z-index
       svg.append('g').attr('class', 'nodes');
 
+      // add labels on the top z-index
       const label = svg.append('g')
         .attr('class', 'labels')
         .selectAll('text')
@@ -114,6 +152,7 @@ class Skills extends React.Component {
           nodes[d.id].radius = radius(nodes[d.id].textWidth);
         });
 
+      // add nodes in the nodes group
       const node = svg.select('g.nodes')
         .selectAll('circle')
         .data(nodes)
@@ -122,28 +161,26 @@ class Skills extends React.Component {
         .style('fill', 'white')
         .attr('r', d => d.radius);
 
-      const simulation = d3.forceSimulation()
+      // create the simulation for our nodes and links
+      d3.forceSimulation()
         .nodes(nodes)
         .force('center', d3.forceCenter(width / 2, height / 2))
-        .force('link', d3.forceLink()
-          .links(links)
-          .distance(l => (l.source.radius + l.target.radius) * 2))
+        .force('link', d3.forceLink().links(links))
         .force('charge', d3.forceManyBody())
-        .force('collision', d3.forceCollide().radius(d => d.radius));
-
-      simulation.on('tick', () => {
-        node
-          .attr('cx', d => (d.x))
-          .attr('cy', d => (d.y));
-        link
-          .attr('x1', d => (d.source.x))
-          .attr('y1', d => (d.source.y))
-          .attr('x2', d => (d.target.x))
-          .attr('y2', d => (d.target.y));
-        label
-          .attr('x', d => (d.x))
-          .attr('y', d => (d.y));
-      });
+        .force('collision', d3.forceCollide().radius(d => d.radius * collisionRadiusModifier))
+        .on('tick', () => {
+          node
+            .attr('cx', d => (d.x))
+            .attr('cy', d => (d.y));
+          link
+            .attr('x1', d => (d.source.x))
+            .attr('y1', d => (d.source.y))
+            .attr('x2', d => (d.target.x))
+            .attr('y2', d => (d.target.y));
+          label
+            .attr('x', d => (d.x))
+            .attr('y', d => (d.y));
+        });
     }
 
     return (
