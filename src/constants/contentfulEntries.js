a7b8eb165b45a6ef22bc74746409f830bbe9c33f
Antonios Karagiannis <antokarag@gmail.com>
2018-02-20 21:27:12 -0500

    feat(constants/contentfulEntries.js): add demos entry

--- src/constants/contentfulEntries.js
+++ src/constants/contentfulEntries.js
@@ -1,6 +1,7 @@
 const contentfulEntries = {
   theme: '1Hhb62W4iMm6yuKI4YMQ0K',
   home: 'xBezDlQxgcMYGiaweq8CK',
+  demos: 'oV9dD3ukW4E8wcSEgu8S2',
   skills: '2ILcmtWssUyW0IC00eakG6',
   contact: '5p5UeMzgZ2Gcwuk0OWUckk',
 };
