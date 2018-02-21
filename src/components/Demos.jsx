6c5b33731d8522d679ca9ca77c8dbbf3f5c56b53
Antonios Karagiannis <antokarag@gmail.com>
2018-02-20 21:29:41 -0500

    feat(components/Demos.jsx): add getDemos prop and invoke in constructor

--- src/components/Demos.jsx
+++ src/components/Demos.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import style from './Demos.pcss';
 
-const Demos = () => (
-  <div className={style.demos}>
-    Demos
-  </div>
-);
+class Demos extends React.Component {
+  constructor(props) {
+    super(props);
+    props.getDemos();
+  }
+
+  render() {
+    return (
+      <div className={style.demos}>
+        Demos
+      </div>
+    );
+  }
+}
+
+Demos.propTypes = {
+  getDemos: PropTypes.func.isRequired,
+};
 
 export default Demos;
